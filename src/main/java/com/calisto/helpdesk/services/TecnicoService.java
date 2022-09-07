b3fe14210dfa185fe24a8d82c068a57a16cbfb13
CiceroCalisto <cicero.calisto97@gmail.com>
2022-09-07 15:54:41 -0300

    Ajuste na atualização de senha

--- src/main/java/com/calisto/helpdesk/services/TecnicoService.java
+++ src/main/java/com/calisto/helpdesk/services/TecnicoService.java
@@ -50,6 +50,11 @@ public class TecnicoService {
 	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
 		objDTO.setId(id);
 		Tecnico oldObj = findById(id);
+		
+		if (!objDTO.getSenha().equals(oldObj.getSenha())) {
+			objDTO.setSenha(encoder.encode(objDTO.getSenha()));
+		}
+		
 		validaPorCFPEEmail(objDTO);
 		oldObj = new Tecnico(objDTO);
 		return repository.save(oldObj);
