c810a02e791ed132be630ae356fc73f9baf10a9d
CiceroCalisto <cicero.calisto97@gmail.com>
2022-09-21 17:11:07 -0300

    Finalizando cliente e tecnicos para o projeto

--- src/main/java/com/calisto/helpdesk/services/DBService.java
+++ src/main/java/com/calisto/helpdesk/services/DBService.java
@@ -28,25 +28,26 @@ public class DBService {
 	private BCryptPasswordEncoder encoder;
 
 	public void instanciaDB() {
-		Tecnico tec1 = new Tecnico(null, "Valdir Cezar", "550.482.150-95", "valdir@mail.com", encoder.encode("123"));
+		Tecnico tec1 = new Tecnico(null, "Cicero Calisto", "550.482.150-95", "cicero@mail.com", encoder.encode("1234"));
 		tec1.addPerfil(Perfil.ADMIN);
-		Tecnico tec2 = new Tecnico(null, "Richard Stallman", "903.347.070-56", "stallman@mail.com", encoder.encode("123"));
-		Tecnico tec3 = new Tecnico(null, "Claude Elwood Shannon", "271.068.470-54", "shannon@mail.com", encoder.encode("123"));
-		Tecnico tec4 = new Tecnico(null, "Tim Berners-Lee", "162.720.120-39", "lee@mail.com", encoder.encode("123"));
-		Tecnico tec5 = new Tecnico(null, "Linus Torvalds", "778.556.170-27", "linus@mail.com", encoder.encode("123"));
+		Tecnico tec2 = new Tecnico(null, "José Augusto Barbosa", "903.347.070-56", "jose@mail.com", encoder.encode("1234"));
+		Tecnico tec3 = new Tecnico(null, "Sergio Paulo Boaventura", "271.068.470-54", "sergio@mail.com", encoder.encode("1234"));
+		Tecnico tec4 = new Tecnico(null, "Marcelo Silva Junior", "162.720.120-39", "marcelo@mail.com", encoder.encode("1234"));
+		Tecnico tec5 = new Tecnico(null, "Kaique Santana", "778.556.170-27", "kaike@mail.com", encoder.encode("1234"));
+		Tecnico tec6 = new Tecnico(null, "Maria de Lourdes Aguiar", "515.652.300-51", "maria@mail.com", encoder.encode("1234"));
 
-		Cliente cli1 = new Cliente(null, "Albert Einstein", "111.661.890-74", "einstein@mail.com", encoder.encode("123"));
-		Cliente cli2 = new Cliente(null, "Marie Curie", "322.429.140-06", "curie@mail.com", encoder.encode("123"));
-		Cliente cli3 = new Cliente(null, "Charles Darwin", "792.043.830-62", "darwin@mail.com", encoder.encode("123"));
-		Cliente cli4 = new Cliente(null, "Stephen Hawking", "177.409.680-30", "hawking@mail.com", encoder.encode("123"));
-		Cliente cli5 = new Cliente(null, "Max Planck", "081.399.300-83", "planck@mail.com", encoder.encode("123"));
+		Cliente cli1 = new Cliente(null, "Silvana Ferreira", "111.661.890-74", "silvana@mail.com", encoder.encode("1234"));
+		Cliente cli2 = new Cliente(null, "Joao Calisto", "322.429.140-06", "joao@mail.com", encoder.encode("1234"));
+		Cliente cli3 = new Cliente(null, "Daniel Rosa", "792.043.830-62", "daniel@mail.com", encoder.encode("1234"));
+		Cliente cli4 = new Cliente(null, "Maria Clara Nascimento", "177.409.680-30", "clara@mail.com", encoder.encode("1234"));
+		Cliente cli5 = new Cliente(null, "Lurdes Dias", "081.399.300-83", "lurdes@mail.com", encoder.encode("1234"));
  
-		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 1", "Teste chamado 1", tec1, cli1);
-		Chamado c2 = new Chamado(null, Prioridade.ALTA, Status.ABERTO, "Chamado 2", "Teste chamado 2", tec1, cli2);
-		Chamado c3 = new Chamado(null, Prioridade.BAIXA, Status.ENCERRADO, "Chamado 3", "Teste chamado 3", tec2, cli3);
-		Chamado c4 = new Chamado(null, Prioridade.ALTA, Status.ABERTO, "Chamado 4", "Teste chamado 4", tec3, cli3);
-		Chamado c5 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 5", "Teste chamado 5", tec2, cli1);
-		Chamado c6 = new Chamado(null, Prioridade.BAIXA, Status.ENCERRADO, "Chamado 7", "Teste chamado 6", tec1, cli5);
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Troca de Fonte de notebook", "Trocar fonte de notebook", tec1, cli1);
+		Chamado c2 = new Chamado(null, Prioridade.ALTA, Status.ABERTO, "Instalar Windows 10 em desktop", "Formatar com Windows 10", tec1, cli2);
+		Chamado c3 = new Chamado(null, Prioridade.BAIXA, Status.ENCERRADO, "Instalar Antivírus Avast", "Instalar Avast no PC", tec2, cli3);
+		Chamado c4 = new Chamado(null, Prioridade.ALTA, Status.ABERTO, "Troca de HD", "Trocar HD queimando por um novo", tec3, cli3);
+		Chamado c5 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Montar PC", "Montar micro com tecnologia AMD", tec2, cli1);
+		Chamado c6 = new Chamado(null, Prioridade.BAIXA, Status.ENCERRADO, "Vender PC", "Colocar PC para venda", tec1, cli5);
 
 		pessoaRepository.saveAll(Arrays.asList(tec1, tec2, tec3, tec4, tec5, cli1, cli2, cli3, cli4, cli5));
 		chamadoRepository.saveAll(Arrays.asList(c1, c2, c3, c4, c5, c6));
